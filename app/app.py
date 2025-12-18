026ed62244517075aac417e39b4809f9b50bfc2a
Sonu64 <sonusantu64@gmail.com>
2025-12-18 19:40:16 +0530

    Added Register Page and Linked Login and Register pages from Main Nav and everywhere - using url_for()

--- app/app.py
+++ app/app.py
@@ -4,6 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template('auth/register.html')
+
+# Register route
+@app.route("/register")
+def register():
+    return render_template('auth/register.html')
+
+# Login route
+@app.route("/login")
+def login():
     return render_template('auth/login.html')
 
 if __name__ == '__main__':
