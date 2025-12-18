a4f30cd693da0ae36104cbf1bdaa36f33404ab1f
Sonu64 <sonusantu64@gmail.com>
2025-12-18 10:46:24 +0530

    Designed Login Page using Custom Tailwind utilities like @components and @apply.

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('core/index.html')
+    return render_template('auth/login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
