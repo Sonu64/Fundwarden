3863ab1ab388d03485a076ec0a3a69c4707f22f5
Sonu64 <sonusantu64@gmail.com>
2025-12-19 19:56:50 +0530

    Add user authentication, registration, and DB migrations
    Implemented user registration and login with password hashing using Flask-Bcrypt. Added client-side validation and AJAX form submission using fetch() for login and registration pages. Integrated Flask-Migrate and Alembic for database migrations, including initial migration for Users table.

--- app/app.py
+++ app/app.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt  # Import Bcrypt
 
-db = SQLAlchemy()
+db = SQLAlchemy() # DB Instance globally
+bcrypt = Bcrypt() # Bcrypt Instance globally
 
 # Factory function
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///./fundwarden.db"
+    
     db.init_app(app)
+    bcrypt.init_app(app)
     
     from app.blueprints.auth.routes import auth # This is the key for Flask to automatically find url_for('core.login') from inside the core folder, and check for @core.route("/login") in routes.py and not in some other file like models.py. Now core. represents the file content of routes.py inside the core folder
     # !!! Route wale file se Blueprint name ko import karo !!!!
     
     app.register_blueprint(auth, url_prefix = "/")
+    
+    migrate = Migrate(app, db)
     
     return app
--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, Flask, request as req
-
-#Import DB Models
+from flask import Blueprint, render_template, Flask, request as req, redirect, url_for
+from flask import jsonify
+#Import DB and DB Models
+from app.app import db, bcrypt
 from app.blueprints.auth.models import User
 
 auth  = Blueprint('auth', __name__)
@@ -9,14 +10,38 @@ auth  = Blueprint('auth', __name__)
 def index():
     return render_template('auth/index.html')
 
-# url_for('core.login') ---> Points to the login route handler of the core BP. Because you don't have any @core.route decorators in models.py, Flask never associates anything in models.py with the "core" endpoint.
-@auth.route("/login", methods = ['GET', 'POST'])
+# url_for('auth.login') ---> Points to the login route handler of the core BP. Because you don't have any @core.route decorators in models.py, Flask never associates anything in models.py with the "core" endpoint.
 
+# When you use Blueprints, Flask "prefixes" every function name with the name of the Blueprint. This is to prevent naming collisions (e.g., if you have an index route in auth and an index route in main). So though login and register handlers of auth are in same file /auth/routes.py, while using url_for() we have to use auth.login, auth.register and auth.index
+#
+@auth.route("/login", methods = ['GET', 'POST'])
 def login():
     if req.method == 'GET':
         return render_template('auth/login.html')
     elif req.method == 'POST':
-        return "Post Request to Login";
+        data = req.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        
+        # Get Row for the Username
+        userRow = User.query.filter(User.username == username).first()
+        # User doesn't exist
+        if not userRow:
+            return jsonify({'error': "Username not Found !"}), 401
+        
+        hashedPassword = userRow.password;
+        # Check hashed password
+        passwordMatched = bcrypt.check_password_hash(hashedPassword, password)
+        # Password doesn't match
+        if not passwordMatched:
+            return jsonify({'error': "Wrong Password !"}), 401
+        
+        else:
+            return jsonify({
+                'message': "Login Successful !",
+                'redirect': url_for('auth.index')
+            }), 200
+        
     else:
         return "Invalid Request !"
     
@@ -27,6 +52,25 @@ def register():
     if req.method == 'GET':
         return render_template('auth/register.html')
     elif req.method == 'POST':
-        return "Post request to Register"
+        data = req.get_json()
+        username = data['username']
+        
+        #Checking for Uniqueness of the Username
+        existingUser = User.query.filter(User.username == username).first()
+        
+        if existingUser:
+            return jsonify({'error': "Username Already Exists !!"}), 409
+        
+        password = data['password']
+        # Hashing Password
+        hashedPassword = bcrypt.generate_password_hash(password)
+        
+        userObject = User(username = username, password = hashedPassword, balance = 1000)
+        db.session.add(userObject)
+        db.session.commit()
+        return jsonify({
+            'message': "Registration Successful !",
+            'redirect': url_for('auth.login')
+        }), 200
     else:
         return "Invalid Request !"
