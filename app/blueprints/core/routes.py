8e25c08f06a5f66ad8357b2838ae9c50018a4fe1
Sonu64 <sonusantu64@gmail.com>
2025-12-25 19:47:30 +0530

    Refactor extend route and template for improved category selection and budget extension handling.

--- app/blueprints/core/routes.py
+++ app/blueprints/core/routes.py
@@ -6,14 +6,16 @@ from app.blueprints.core.models import Categories
 # Import Authentication modules from flask_login
 from flask_login import login_user, logout_user, current_user, login_required
 
-core  = Blueprint('core', __name__)
+core = Blueprint('core', __name__)
 
-@core.route('/') # /app
+
+@core.route('/')  # /app
 @login_required
 def index():
-    return render_template('/core/index.html', user = current_user)
+    return render_template('/core/index.html', user=current_user)
 
-@core.route('add', methods = ['GET', 'POST'])
+
+@core.route('add', methods=['GET', 'POST'])
 @login_required
 def add():
     if req.method == 'GET':
@@ -29,103 +31,131 @@ def add():
         return redirect(url_for('core.index'))
     else:
         return "Invalid Request !"
-    
-    
+
+
 @core.route('allocator')
 @login_required
 def allocator():
-    categories = Categories.query.filter(Categories.userID == current_user.id)
-    categoriesList = []
-    for c in categories:
-        categoriesList.append({'name':c.name, 'budget':c.budget})
-    return render_template('core/allocator.html', categoriesList = categoriesList, category = "", budget = "")
+    categoriesList = giveCategoriesListOfDicts()
+    return render_template('core/allocator.html',
+                           categoriesList=categoriesList,
+                           category="",
+                           budget="")
 
 
-@core.route("createCategory", methods = ['GET', 'POST'])
+@core.route("createCategory", methods=['GET', 'POST'])
 @login_required
 def createCategory():
     if req.method == 'GET':
         # Possible only through Redirects to url_for('core.createCategory')
-        return redirect(url_for('core.allocator')) # category and budget handled by IF jinja syntax in core/allocator.html, same for categoriesList, these values can only be added by render_template, not redirect()
+        return redirect(
+            url_for('core.allocator')
+        )  # category and budget handled by IF jinja syntax in core/allocator.html, same for categoriesList, these values can only be added by render_template, not redirect()
     elif req.method == 'POST':
         categoryName = req.form.get('category')
         budget = req.form.get('budget')
-        print(categoryName, budget)
+        categoriesList = giveCategoriesListOfDicts()
+
         if (categoryName.strip() == ""):
             flash("Please Provide a Category name !", "danger")
-            categories = Categories.query.filter(Categories.userID == current_user.id)
-            categoriesList = []
-            for c in categories:
-                categoriesList.append({'name':c.name, 'budget':c.budget})
-            return render_template('core/allocator.html', categoriesList = categoriesList, category = "", budget = budget)
-        
+            return render_template('core/allocator.html',
+                                   categoriesList=categoriesList,
+                                   category="",
+                                   budget=budget)
+
         if (int(budget) == 0):
             flash("Please Provide a Budget amount !", "danger")
-            categories = Categories.query.filter(Categories.userID == current_user.id)
-            categoriesList = []
-            for c in categories:
-                categoriesList.append({'name':c.name, 'budget':c.budget})
-            return render_template('core/allocator.html', categoriesList = categoriesList, category = categoryName, budget = "")
-        
+            return render_template('core/allocator.html',
+                                   categoriesList=categoriesList,
+                                   category=categoryName,
+                                   budget="")
+
         categoryName = categoryName.strip()
-        
+
         userID = current_user.id
         spent = 0
         remaining = budget
-        
-        categoryObj = Categories(userID = userID, name = categoryName, budget = budget, spent = spent, remaining = remaining)
-        
+
+        categoryObj = Categories(userID=userID,
+                                 name=categoryName,
+                                 budget=budget,
+                                 spent=spent,
+                                 remaining=remaining)
+
         db.session.add(categoryObj)
         db.session.commit()
-        
+
         flash(f"Successfuly created Category {categoryName} !", "success")
         return redirect(url_for('core.allocator'))
-        
+
     else:
         return "Invalid Request !"
 
 
-
-
-@core.route('extend', methods = ['GET', 'POST'])
+@core.route('extend', methods=['GET', 'POST'])
 @login_required
 def extend():
+    categoriesList = giveCategoriesListOfDicts()
     if req.method == 'GET':
-        categories = Categories.query.filter(Categories.userID == current_user.id)
-        categoriesList = []
-        for c in categories:
-            categoriesList.append({'name':c.name, 'budget':c.budget})
-        return render_template('core/extend.html', categories = categoriesList)
+        return render_template('core/extend.html',
+                               categories=categoriesList,
+                               category="",
+                               extension="")
     elif req.method == 'POST':
         category = req.form.get('category')
         extension = req.form.get('extension')
-        print(category, extension)
-        filteredCategory = Categories.query.filter(Categories.name==category, Categories.userID==current_user.id).first() 
+
+        if (not category):
+            flash("Please Choose a Category !", "danger")
+            return render_template('core/extend.html',
+                                   categories=categoriesList,
+                                   category="",
+                                   extension=extension)
+        if (extension == "0" or not extension):
+            flash("Please Give a Valid Extension amount !", "danger")
+            return render_template('core/extend.html',
+                                   categories=categoriesList,
+                                   category=category,
+                                   extension="")
+
+        filteredCategory = Categories.query.filter(
+            Categories.name == category,
+            Categories.userID == current_user.id).first()
         # .first() or .all() is ALWAYS needed !!!!!!
         # .all() gives a List of Category Objects ---> all objects are Live Copies from the DB
         # .first() gives a Single Category Object ---> all objects are Live Copies from the DB
-        
+
         if not filteredCategory:
-            return "Category Not found !"
-        
+            flash("Error while fetching category name !", "danger")
+            return redirect(url_for('core.allocator'))
+
         filteredCategory.budget += int(extension)
-        
+
         # db.session.add(filteredCategory) # No need as filteredCategory is a Live Copy of the Row, though
         # db.session.commit() is needed as below
-        db.session.commit()
-        
-        return redirect(url_for('core.allocator'))
+        try:
+            db.session.commit()
+            flash(f"Extended your Budget for {filteredCategory.name} !",
+                  "success")
+            return redirect(url_for('core.allocator'))
+        except:
+            flash("Some Error Occured !", "danger")
+            return redirect(url_for('core.allocator'))
     else:
         return "Invalid Request"
-    
-
-
-
-
-
 
 
 @core.route('tracker')
 @login_required
 def tracker():
     return render_template('core/tracker.html')
+
+
+# METHODS IRRESPECTIVE OF ROUTES #
+def giveCategoriesListOfDicts():
+    """Give the List of Dictionaries containing each category and the respective allocated budget"""
+    categories = Categories.query.filter(Categories.userID == current_user.id)
+    categoriesList = []
+    for c in categories:
+        categoriesList.append({'name': c.name, 'budget': c.budget})
+    return categoriesList
